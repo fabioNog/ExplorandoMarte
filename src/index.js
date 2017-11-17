b7ed0da64a27fec62e2c1f3cf3ca37cd8310f1c0
Fabio <fabio.carmo@computacao.ufla.br>
2017-11-17 11:06:49 -0200

    Voltando para o frontend inicial

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { combineReducers, createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 // Testando o Field
-import counterReducer from './TesteRedux/counterReducer'
-import Counter from './TesteRedux/counter'
+// import counterReducer from './TesteRedux/counterReducer'
+// import Counter from './TesteRedux/counter'
 
-const reducers = combineReducers({
-     counter: counterReducer 
-})
+// const reducers = combineReducers({
+//      counter: counterReducer 
+// })
 
 ReactDOM.render(
-    <Provider store={createStore(reducers)}>
-        <Counter />
-    </Provider>, document.getElementById('root'));
+    // <Provider store={createStore(reducers)}>
+    //     <Counter />
+    // </Provider>, document.getElementById('root'));
+    <App/>,document.getElementById('root'));
+        
+
 registerServiceWorker();
